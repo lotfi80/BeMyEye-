2909d6110cba7932fa55651a05b6094b3265f235
lotfi80 <salioslim@hotmail.de>
2024-08-20 16:23:44 +0200

    added order for routes and controllers

---
+++ Server/src/libs/db.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL ;
+const connectDB = async () => {
+    try{
+        if(!MONGODB_URL){
+            throw new Error('MONGODB_URL is not defined');
+        }
+        await mongoose.connect(MONGODB_URL)
+        console.log("connected to mongoDB");}
+        catch (err){
+            console.error("Error connecting to mongoDB", err);
+            process.exit(1);
+        
+        }
+    }
+ 
+    export default connectDB;
+    
