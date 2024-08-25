da190203c0ad80511d873d4ac2e198a6d547d08a
lotfi80 <salioslim@hotmail.de>
2024-08-25 18:48:04 +0200

    localhost änderung commit um zu weckseln auf nath branche

--- Server/main.ts
+++ Server/main.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieparser());
 
 app.use(cors({
-  origin: 'http://localhost:5174', 
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true 
