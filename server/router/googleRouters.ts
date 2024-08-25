27c8dc1b89030a6da13c348c7c2bdb0dd62756d6
Nath-DCI <natalia.zharova@dci-student.org>
2024-08-25 19:43:05 +0200

    clearing

--- server/router/googleRouters.ts
+++ server/router/googleRouters.ts
@@ -4,10 +4,6 @@ import { handleGoogleCallback } from "../service/passport-service";
 
 const googleRouter = express.Router();
 
-// googleRouter.get(
-//   "/auth/google",
-//   passport.authenticate("google", { scope: ["profile", "email"] })
-// );
 googleRouter.get("/auth/google", (req, res, next) => {
   console.log("Initiating Google authentication process");
   passport.authenticate("google", { scope: ["profile", "email"] })(
