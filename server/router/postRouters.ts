6f3fada29eee31c3d0e44674e5e5ac3ba975462d
lotfi80 <salioslim@hotmail.de>
2024-08-29 14:36:09 +0200

    getallPosts in place and is working . have to fix the image of the posts in the home page

--- server/router/postRouters.ts
+++ server/router/postRouters.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import { getPosts } from "../controllers/post-controller";
 // import { getUserPosts } from "../controllers/post";
 import { createPost } from "../controllers/post-controller";
 import { imagesUpload } from "../service/multer-service";
@@ -8,5 +9,6 @@ const postRouter = express.Router();
 const upload = multer({ dest: "uploads/" });
 // postRouter.get("/get", getUserPosts);
 postRouter.post("/create", imagesUpload.single("postImages"), createPost);
+postRouter.get("/",getPosts);
 
 export default postRouter;
